84de0aa35c44f141adf36875c7f5b5b1a39d6303
alfredsn <alfred.nainggolan00@gmail.com>
2024-06-17 15:54:14 +0700

    add new permissions to manifest at app module and update ViewModelFactory

--- app/src/main/java/com/example/triad/factory/ViewModelFactory.kt
+++ app/src/main/java/com/example/triad/factory/ViewModelFactory.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.triad.viewmodel.MainViewModel
 
+@Suppress("UNCHECKED_CAST")
 class ViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return MainViewModel() as T
+        return when {
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                MainViewModel() as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class")
         }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
